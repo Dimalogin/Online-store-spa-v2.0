0dd5407fbc3db744d4a2814e3be95a2b2c45f326
DimaLoginovskiy <remen.2000@mail.ru>
2024-09-30 01:29:45 +0300

    feat: adding gallery brands footer

---
+++ assets/pages/home/scripts/slider.js
@@ -0,0 +1,54 @@
+export default class MainSliderWrapper {
+  #sliderWrapperArrowPrev = null;
+  #sliderWrapperArrowNext = null;
+  #slidesWrapperList = null;
+  #slidesWrapperSlide = null;
+
+  slideIndex = 0;
+
+  constructor() {
+    this.#initTemplate();
+    this.#bindListener();
+
+    this.#showSlides();
+  }
+
+  #initTemplate() {
+    this.#sliderWrapperArrowPrev = document.querySelector(
+      ".slider-wrapper__arrow--prev"
+    );
+    this.#sliderWrapperArrowNext = document.querySelector(
+      ".slider-wrapper__arrow--next"
+    );
+    this.#slidesWrapperList = document.querySelector(".slides-wrapper__list");
+    this.#slidesWrapperSlide = document.querySelector(".slides-wrapper__slide");
+  }
+
+  #bindListener() {}
+
+  #showSlides() {
+    const slideWidth = this.#slidesWrapperSlide.clientWidth;
+    this.#slidesWrapperList.scrollLeft += slideWidth;
+
+    this.slideIndex++;
+
+    if (this.slideIndex === 4) {
+      this.slideIndex = 0;
+      this.#slidesWrapperList.scrollLeft = 0;
+    }
+
+    setTimeout(this.#showSlides.bind(this), 3000);
+  }
+
+  #switchSlideToNext() {
+    const slideWidth = this.#slidesWrapperSlide.clientWidth;
+    this.#slidesWrapperList.scrollLeft += slideWidth;
+    this.slideIndex++;
+  }
+
+  #switchSlideToPrev() {
+    this.slideIndex--;
+    const slideWidth = this.#slidesWrapperSlide.clientWidth;
+    this.#slidesWrapperList.scrollLeft -= slideWidth;
+  }
+}
--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -2,6 +2,11 @@ import HeaderDropdown from "./header/dropdown-menu.js";
 import HeaderSideNavigation from "./header/side-navigation.js";
 import HeaderSideDropdownMenu from "./header/side-dropdown-menu.js";
 
+import MainSliderWrapper from "../pages/home/scripts/slider.js";
+
 const headerDropdown = new HeaderDropdown();
 const headerSideNavigation = new HeaderSideNavigation();
 const headerSideDropdownMenu = new HeaderSideDropdownMenu();
+
+// Slider
+const mainSliderWrapper = new MainSliderWrapper();
