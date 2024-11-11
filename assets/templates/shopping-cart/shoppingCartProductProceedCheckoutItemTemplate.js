85429952a07458d815600593f5df4fb40922b65a
DimaLoginovskiy <remen.2000@mail.ru>
2024-11-11 10:52:45 +0300

    feat: create template for modal window order complete

---
+++ assets/templates/shopping-cart/shoppingCartProductProceedCheckoutItemTemplate.js
@@ -0,0 +1,69 @@
+const shoppingCartProductProceedCheckoutItemTemplate =
+  document.createElement("template");
+
+shoppingCartProductProceedCheckoutItemTemplate.innerHTML = `
+<li
+                    class="shopping-cart-products-proceed-to-checkout-modal-window-order-summary__item"
+                  >
+                    <div
+                      class="shopping-cart-products-proceed-to-checkout-modal-window-order-summary__top"
+                    >
+                      <div
+                        class="shopping-cart-products-proceed-to-checkout-modal-window-order-summary-item__icon"
+                      >
+                        <img
+                          src="./assets/images/products/for-woman/dresses/woman-dress-1.png"
+                          alt="dress"
+                        />
+                        <span
+                          class="shopping-cart-products-proceed-to-checkout-modal-window-order-summary-item__quantity"
+                          >10</span
+                        >
+                      </div>
+                      <div
+                        class="shopping-cart-products-proceed-to-checkout-modal-window-order-summary-item__content"
+                      >
+                        <div
+                          class="shopping-cart-products-proceed-to-checkout-modal-window-order-summary-item-content__title"
+                        >
+                          Lorem ipsum, dolor sit amet consectetur Lorem
+                        </div>
+                        <div
+                          class="shopping-cart-products-proceed-to-checkout-modal-window-order-summary-item-content__parameters"
+                        >
+                          <div
+                            class="shopping-cart-products-proceed-to-checkout-modal-window-order-summary-item-content__color"
+                          >
+                            <span
+                              class="shopping-cart-products-proceed-to-checkout-modal-window-order-summary-item-content-color__text"
+                              >Color:</span
+                            >
+                            <span
+                              class="shopping-cart-products-proceed-to-checkout-modal-window-order-summary-item-content-color__value"
+                              >Black</span
+                            >
+                          </div>
+                          <div
+                            class="shopping-cart-products-proceed-to-checkout-modal-window-order-summary-item-content__size"
+                          >
+                            <span
+                              class="shopping-cart-products-proceed-to-checkout-modal-window-order-summary-item-content-size__text"
+                              >Size:</span
+                            >
+                            <span
+                              class="shopping-cart-products-proceed-to-checkout-modal-window-order-summary-item-content-size__value"
+                              >L</span
+                            >
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div
+                      class="shopping-cart-products-proceed-to-checkout-modal-window-order-summary-item__price"
+                    >
+                      $100
+                    </div>
+                  </li>
+`;
+
+export default shoppingCartProductProceedCheckoutItemTemplate;
